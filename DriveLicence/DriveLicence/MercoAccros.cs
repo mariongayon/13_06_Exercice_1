bc1d24992ba73fdb1f2db568ec2e6943b084dcc0
Marion Gayon <mariongayon@gmail.com>
2022-06-10 13:16:00 +0200

    add hierarchie

---
+++ DriveLicence/DriveLicence/MercoAccros.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveLicence
+{
+    class MercoAccros : ITruck
+    {
+        TruckTemplate implementation;
+
+        public MercoAccros(string licencePlate, float maxLegalSpeed, float maxFreight) {
+
+            implementation = new TruckTemplate(licencePlate, maxLegalSpeed, maxFreight);
+        }
+
+        public string LicencePlate()
+        {
+            return implementation.LicencePlate();
+        }
+
+        public float MaxLegalSpeed()
+        {
+            return implementation.MaxLegalSpeed();
+        }
+
+        public float MaxFreight()
+        {
+            return implementation.MaxFreight();
+        }
+    }
+}
---
+++ DriveLicence/DriveLicence/TruckTemplate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveLicence
+{
+    public class TruckTemplate : ITruck 
+    {
+        string licencePlate;
+        float maxLegalSpeed;
+        float maxFreight;
+
+        public TruckTemplate(string licencePlate, float maxLegalSpeed, float maxFreight)
+        {
+            this.licencePlate = licencePlate;
+            this.maxLegalSpeed = maxLegalSpeed;
+            this.maxFreight = maxFreight;
+        }
+
+        public string LicencePlate()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public float MaxLegalSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float MaxFreight()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
